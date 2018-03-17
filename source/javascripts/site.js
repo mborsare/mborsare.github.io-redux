a8cc15ef238f01b58c6268a4eee84bc876e761a9
Mike Borsare <mborsare@gmail.com>
2018-03-17 14:54:21 -0400

    --wip-- [skip ci]

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,8 +1,16 @@
 // This is where it all goes :)
 
 $(document).ready(function() {
-
   console.log('sup');
+
+  var $root = $('html, body');
+  $('a[href^="#"]').click(function () {
+    $root.animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+
+    return false;
+  });
 
   // $('.introduction').hover(function() {
   //   $(this).toggleClass('revealed');
